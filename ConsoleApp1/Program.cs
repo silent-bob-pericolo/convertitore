098a0d2a49ef52b5ff9453091daa314080647453
alessandro <m.cherubini52@gmail.com>
2020-11-03 10:33:57 +0100

    codice convertitore

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            int numero;
+            int resto;
+            string numeroConvertito = "";
+            int divisore;
+            Console.WriteLine("inserisci il numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inserisci il divisore");
+            divisore = Convert.ToInt32(Console.ReadLine());
+            while (numero > 0)
+            {
+                resto = numero % divisore;
+                numero = numero / 2;
+                numeroConvertito = Convert.ToString(resto) + numeroConvertito;
+            }
+            Console.WriteLine($"{numeroConvertito}");
+            Console.ReadLine());
         }
+    
     }
 }
